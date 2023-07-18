0aa1e075172d2b74bfd7dd9d4bed273ca5b63082
Jummit <jummit@web.de>
2023-07-18 23:18:29 +0200

    Add wren support

---
+++ runtime/queries/wren/textobjects.scm
@@ -0,0 +1,13 @@
+; TODO: @correctness fix these
+(class_definition
+  (class_body) @class.inside) @class.around
+
+(method_definition
+  body: (_) @function.inside) @function.around
+
+(parameter_list
+  ((_) @parameter.inside . ","? @parameter.around) @parameter.around)
+
+(comment) @comment.inside
+
+(comment)+ @comment.around
